b7c87efac0f90330ec53db773748eea8831d8fa4
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-21 08:15:57 +0200

    add resultSection component

---
+++ src/components/ResultSection.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="resultsection mt-5">
+          <h2 class="text-left h3 mb-4 mt-5"><strong>Dans le cadre de votre projet, vous souhaitez :</strong></h2>
+              <div class="row" >
+                <!-- <div class="row" v-for="aide in aides" :key="aide.id" > -->
+                    <!-- <div class="col-sm-4 mb-4">
+                        <div class="col-sm bgGrey p-4">
+                            <p class="mb-5 text-left"><strong>{{ aide.name }}</strong></p>
+                            <button class="moreInfos text-left" @click="goToAide(aide.id)">Obtenir des informations →</button>
+                        </div>
+                    </div> -->
+                    <div class="col-sm-4 mb-4">
+                        <div class="col-sm bgGrey p-4">
+                            <p class="mb-5 text-left"><strong>Développer la dématérialisation de qualité des services aux usagers</strong></p>
+                            <button class="moreInfos text-left">Obtenir des informations →</button>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 mb-4">
+                        <div class="col-sm bgGrey p-4">
+                            <p class="mb-5 text-left"><strong>Développer la dématérialisation de qualité des services aux usagers</strong></p>
+                            <button class="moreInfos text-left">Obtenir des informations →</button>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 mb-4">
+                        <div class="col-sm bgGrey p-4">
+                            <p class="mb-5 text-left"><strong>Développer la dématérialisation de qualité des services aux usagers</strong></p>
+                            <button class="moreInfos text-left">Obtenir des informations →</button>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 mb-4">
+                        <div class="col-sm bgGrey p-4">
+                            <p class="mb-5 text-left"><strong>Développer la dématérialisation de qualité des services aux usagers</strong></p>
+                            <button class="moreInfos text-left">Obtenir des informations →</button>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 mb-4">
+                        <div class="col-sm bgGrey p-4">
+                            <p class="mb-5 text-left"><strong>Développer la dématérialisation de qualité des services aux usagers</strong></p>
+                            <button class="moreInfos text-left">Obtenir des informations →</button>
+                        </div>
+                    </div>
+            </div>
+              
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ResultSection"
+
+    }
+</script>
+
+<style>
+
+    .bgGrey {
+        background-color: #f7f5f5;
+        color: #2c3e50;
+    }
+
+    .moreInfos {
+        color: #408669;
+        background-color: inherit;
+        border: none;
+    }
+
+</style>
--- src/views/francerelance.vue
+++ src/views/francerelance.vue
@@ -7,7 +7,7 @@
 
       <div class="TopSelectionDiv mt-5">
           <h2 class="text-left h3 mb-4 mt-5"><strong>Découvrez tous les financements auxquels vous pouvez faire appel :</strong></h2>
-          <div class="row justify-content-center">
+          <div class="row">
             <TopSelection :title="topSelection1" :focus="true"></TopSelection>
             <TopSelection :title="topSelection2" :focus="false"></TopSelection>    
           </div>
@@ -15,25 +15,27 @@
 
       <div class="DateSelectionDiv mt-5">
           <h2 class="text-left h3 mb-4 mt-5"><strong>Quelle est l'échéance de votre projet ?</strong></h2>
-          <div class="row justify-content-center">
+          <div class="row">
             <DateSelection :title="dateSelection1" :date="date1" :focus="false"></DateSelection>
             <DateSelection :title="dateSelection2" :date="date2" :focus="false"></DateSelection>    
           </div>
       </div>
 
+      <ResultSection></ResultSection>
   </div>
 </template>
 
 <script>
-  import IntroSection from "@/components/introsection";
-  import SearchBar from "@/components/searchbar";
-  import TopSelection from "@/components/topselection";
-  import DateSelection from "@/components/dateselection";
+  import IntroSection from "@/components/IntroSection";
+  import SearchBar from "@/components/SearchBar";
+  import TopSelection from "@/components/TopSelection";
+  import DateSelection from "@/components/DateSelection";
+  import ResultSection from "@/components/ResultSection";
 
   export default {
       name: "FranceRelance",
 
-      components: { IntroSection, TopSelection, SearchBar, DateSelection },
+      components: { IntroSection, TopSelection, SearchBar, DateSelection, ResultSection },
 
       data() {
         return {
