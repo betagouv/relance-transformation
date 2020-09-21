cb989759503872733db2ad3799603de850622aa9
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-21 12:38:34 +0200

    add video tag

--- src/components/introsection.vue
+++ src/components/introsection.vue
@@ -5,9 +5,11 @@
               <h1 class="greenTitle text-left mb-4"><strong>Administrations : bénéficiez du volet « Mise à niveau numérique de l'État et des territoires »</strong></h1>
               <div class="row">
                 <p class="text-left col-sm-8 mr-5 h3">Votre projet doit contribuer à la relance de l'économie française, notamment des entreprises, et doit produire des résultats concrets sous 18 mois.</p>
-                <div class="video col-sm-3 embed-responsive embed-responsive-16by9">
-                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fTP37ETteOQ" allowfullscreen></iframe>
-                  <p class="text-justify"><small>Le mot d'Amélie de Montchalin</small></p>
+                <div class="video col-sm-3">
+                    <div class="embed-responsive embed-responsive-16by9 col-sm">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fTP37ETteOQ" allowfullscreen></iframe>
+                    </div>
+                    <p class="text-center mt-2 fontbrown">Le mot d'Amélie de Montchalin</p>
                 </div>
               </div>
           </div>
@@ -24,5 +26,9 @@
 <style>
     .greenTitle {
         color: #408669;
+    }
+
+    .fontbrown {
+        color: #69695b;
     }
 </style>
