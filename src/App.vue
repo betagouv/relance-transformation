93e0ea1500e7eb73e1307c55cf43d2d4ae7ed897
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-22 19:39:35 +0200

    start integration maquette rf-grid

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,17 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Marianne','Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+@font-face {
+    font-family: "Marianne";
+    src: local("Marianne"),
+    url(../public/static/fonts/Marianne-Regular.woff) format("webopen");
+  }
 </style>
