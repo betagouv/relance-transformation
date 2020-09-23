f493aeecb34624c3245038c6165c89d918fcc452
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-23 12:17:34 +0200

    create VideoPage

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import FranceRelance from '@/views/francerelance.vue'
 import Aides from '@/views/Aides.vue'
+import Video from '@/views/Video.vue'
 
 Vue.use(Router)
 
@@ -12,13 +13,18 @@ export default new Router({
       name: 'FranceRelance',
       component: FranceRelance,
     },
+    {
+      path: '/Video',
+      name: 'video',
+      component: Video,
+    },
     // {
     //   path: '/Aides/:id',
     //   name: 'Aides',
     //   component: Aides,
     // },
     {
-      path: '/Aides/:name/:id',
+      path: '/Aides/:slug/:id',
       name: 'Aides',
       component: Aides,
     },
