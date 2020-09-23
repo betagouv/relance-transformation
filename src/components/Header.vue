bc38260ad75036126a0f12c1ad4609c658a5bd59
Ugo <jeanmichelpaul@icloud.com>
2020-09-23 18:33:40 +0200

    Edit header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="rf-header rf-margin-bottom-5N">
         <div class="rf-container">
-            <div class="rf-header__body">
+            <!-- <div class="rf-header__body">
                 <div class="rf-header__brand">
                     <a class="rf-logo" href="./">
                         <span class="rf-logo__title">Ministère
@@ -10,12 +10,15 @@
                             <br>publiques</span>
                     </a>
                 </div>
-            </div>
+            </div> -->
+            <a class="rf-logo--custom" href="./">
+              <img class="rf-logo__image--custom" src="@/assets/MarqueMTFP.svg" alt="Ministère de la Transformation et de la Fonction publiques. Liberté Égalité Fraternit. - Retour à l'accueil">
+            </a>
             <div class="rf-header__navbar">
                 <nav class="rf-nav" role="navigation" aria-label="Menu principal" id="header-navigation">
                     <ul class="rf-nav__list">
                         <li class="rf-nav__item">
-                            <button class="rf-btn" aria-expanded="false" aria-controls="rf-nav-item-0">La
+                            <button class="rf-btn rf-btn--cutom" aria-expanded="false" aria-controls="rf-nav-item-0">La
                                 ministre</button>
                             <div class="rf-menu" id="rf-nav-item-0">
                                 <ul class="rf-menu__list">
@@ -30,7 +33,7 @@
                             </div>
                         </li>
                         <li class="rf-nav__item">
-                            <button class="rf-btn" aria-expanded="false" aria-controls="rf-nav-item-1">Le
+                            <button class="rf-btn rf-btn--cutom" aria-expanded="false" aria-controls="rf-nav-item-1">Le
                                 ministère</button>
                             <div class="rf-menu" id="rf-nav-item-1">
                                 <ul class="rf-menu__list">
@@ -41,9 +44,9 @@
                                 </ul>
                             </div>
                         </li>
+                        <li class="rf-nav__item"><a class="rf-link" href="#" target="_self">Espace presse</a></li>
                         <li class="rf-nav__item rf-nav__item--active"><a class=" rf-link" href="#" target="_self">
                             France Relance</a></li>
-                        <li class="rf-nav__item"><a class="rf-link" href="#" target="_self">Espace presse</a></li>
                     </ul>
                 </nav>
             </div>
@@ -68,11 +71,28 @@
     box-shadow: none;
     border-top: 1px solid #e7e7e7;
     border-bottom: 1px solid #e7e7e7;
+    width: 100vw;
   }
 
-  .rf-logo {
+  .rf-header__navbar {
+    margin: 0px;
+  }
 
-    height: 128px;
+  .rf-logo__image--custom {
+    height: 158px;
+    margin: 30px 30px 30px 0px;
+  }
+
+  .rf-btn--cutom {
+    padding: 16px 16px 16px 12px !important;
+  }
+
+  .rf-nav__item {
+    margin: 0 12px 0 0px !important;
+  }
+
+  .rf-link {
+    margin: 0 -4px 0 -4px !important;
   }
 
 </style>
