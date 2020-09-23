cc3b637960da7e460dd6ca1392c08bf648ca67e8
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-23 22:00:44 +0200

    add axios functions to filter results, create slot in ResultSection component

---
+++ src/components/ResultSection.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="">
+          <h2>Dans le cadre de votre projet, vous souhaitez :</h2>
+          <slot></slot>
+          <slot name="resultCards"></slot>             
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ResultSection"
+
+    }
+</script>
+
+<style>
+
+    .bgGrey {
+        background-color: #f7f5f5;
+        color: #2c3e50;
+    }
+
+    .moreInfos {
+        color: #408669;
+        background-color: inherit;
+        border: none;
+    }
+
+    a {
+        text-decoration: none !important;
+        box-shadow: none;
+    }
+
+    .font16 {
+        font-size: 16px;
+    }
+
+    .font14 {
+        font-size: 14px;
+    }
+
+</style>
