192f1a25d97638215882fe10d14d11851db64bdd
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2020-09-24 12:06:31 +0200

    RWD introsection

--- src/components/IntroSection.vue
+++ src/components/IntroSection.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="rf-grid-row rf-grid-row--top ">
-        <div class="rf-col-1">
+    <div class="rf-grid-row rf-grid-row--top">
+        <div class="rf-col-xs-12 rf-col-md-2 rf-col-lg-2 rf-col-xl-2">
             <img src="@/assets/FranceRelance.png" alt="France Relance" width="70px">
         </div>
-        <div class="rf-col-11">
+        <div class="rf-col">
             <div>
                 <h1 class="greenTitle">Administrations : bénéficiez du volet « Mise à niveau numérique de l'État et des territoires »</h1>
             </div>
             <div class="rf-grid-row">
-                <div class="rf-col-8">
+                <div class="rf-col-sm-8 rf-col-xs-12">
                     <p>Votre projet doit contribuer à la relance de l'économie française, notamment des entreprises, et doit produire des résultats concrets sous 18 mois.</p>
                 </div>
-                <div class="rf-col-3">
+                <div class="rf-col-sm-4 rf-col-xs-12">
+                <div class="thumbs">
                     <a href="" class="rf-col"><img src="@/assets/PhotoAmelieDeMontchalin.png" alt="la vidéo d'Amélie de Montchalin" width="182px"/></a>
                     <p class="fontGrey">Le mot d'Amélie de Montchalin</p>
+                </div>
                 </div>
             </div>
         </div>
@@ -29,11 +31,17 @@
 </script>
 
 <style>
-    .rf-col-11 {
-        padding-left: 0.5em;;
+    div.rf-grid-row--top div {
+        text-align: center;
+    }
+    div.rf-grid-row div.rf-col div {
+        text-align: left;
+    }
+    div.rf-grid-row .rf-col-xs-12 .thumbs {
+        text-align: center;
     }
     .greenTitle {
-        color: #107449;
+        color: #33A964;
         margin-top:0;
         font-size: 2rem;
     }
