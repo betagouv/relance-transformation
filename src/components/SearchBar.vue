f1bd8fd85d953083a12891035bfa2cc497b5fdd3
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-24 15:28:08 +0200

    add connected searchbar in Search.vue & Home.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,14 +3,7 @@
         <div class="rf-col-11">
             <h2 class="font22 text-left mb-4"><strong>Rechercher un financement :</strong></h2>
         </div>
-        <form action="" class="rf-col-11">
-            <div class="input-group mb-3">
-              <input type="search" class="form-control border-bottom-green formresearch p-4" placeholder="Rechercher un mot clé, une expression, une référence..." aria-describedby="basic-addon2">
-              <div class="input-group-append">
-                <button class="font20 btn my-2 greenButton my-sm-0 input-group-text pl-4 pr-4" type="submit">Rechercher</button>
-              </div>
-            </div>
-        </form>          
+        <slot></slot>   
       </div>
 </template>
 
