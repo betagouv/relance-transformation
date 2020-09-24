2b0e2fbd6d2f98324ec225dd73f2d527a112fc54
Ugo <jeanmichelpaul@icloud.com>
2020-09-24 19:33:17 +0200

    Add subfooter

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,79 +1,85 @@
 <template>
-    <footer class="rf-footer  rf-margin-top-5N" role="contentinfo" id="">
-        <div class="rf-container">
-            <div class="rf-footer__body">
-                <div class="rf-footer__brand">
-                    <a class="rf-logo" href="./" >
-                        <span class="rf-logo__title">Ministère
-                            <br>de la transformation
-                            <br>et de la fonction
-                            <br>publiques</span>
-                    </a>
-                </div>
-                <div class="rf-footer__content">
-                    <div class="rf-footer__content-desc">
-                        <div class="rf-footer__content-desc-column">
-                            <a href="https://www.fonction-publique.gouv.fr/">
-                                fonction-publique.gouv.fr
-                            </a>
-                            <p class="rf-footer__content-desc-entity-name">
-                                Direction Générale de l’Administration et de la Fonction Publique (DGAFP)
-                            </p>
-                        </div>
-                        <div class="rf-footer__content-desc-column">
-                            <a href="https://www.numerique.gouv.fr/">
-                                numerique.gouv.fr
-                            </a>
-                            <p>
-                                Direction Interministérielle du Numérique (DINUM)
-                            </p>
-                        </div>
-                        <div class="rf-footer__content-desc-column">
-                            <a href="https://www.modernisation.gouv.fr/">
-                                modernisation.gouv.fr
-                            </a>
-                            <p>
-                                Direction Interministérielle de la Transformation Publique (DITP)
-                            </p>
-                        </div>
+    <div>
+        <div class="subfooter rf-margin-top-15N">
+            <p class="rf-text">Ce site est conçu et édité en partenariat avec <a href="https://aides-territoires.beta.gouv.fr" target="_blank">Aides-Territoires</a></p>
+            <a href="https://aides-territoires.beta.gouv.fr" target="_blank"><img src="@/assets/LogoAT.svg" alt="Aides-Territoires - Lien vers le site"></a>
+        </div>
+        <footer class="rf-footer" role="contentinfo" id="">
+            <div class="rf-container">
+                <div class="rf-footer__body">
+                    <div class="rf-footer__brand">
+                        <a class="rf-logo" href="./" >
+                            <span class="rf-logo__title">Ministère
+                                <br>de la transformation
+                                <br>et de la fonction
+                                <br>publiques</span>
+                        </a>
                     </div>
-                    <ul class="rf-footer__content-links">
+                    <div class="rf-footer__content">
+                        <div class="rf-footer__content-desc">
+                            <div class="rf-footer__content-desc-column">
+                                <a href="https://www.fonction-publique.gouv.fr/">
+                                    fonction-publique.gouv.fr
+                                </a>
+                                <p class="rf-footer__content-desc-entity-name">
+                                    Direction Générale de l’Administration et de la Fonction Publique (DGAFP)
+                                </p>
+                            </div>
+                            <div class="rf-footer__content-desc-column">
+                                <a href="https://www.numerique.gouv.fr/">
+                                    numerique.gouv.fr
+                                </a>
+                                <p>
+                                    Direction Interministérielle du Numérique (DINUM)
+                                </p>
+                            </div>
+                            <div class="rf-footer__content-desc-column">
+                                <a href="https://www.modernisation.gouv.fr/">
+                                    modernisation.gouv.fr
+                                </a>
+                                <p>
+                                    Direction Interministérielle de la Transformation Publique (DITP)
+                                </p>
+                            </div>
+                        </div>
+                        <ul class="rf-footer__content-links">
+                            <li>
+                                <a href="https://legifrance.gouv.fr">legifrance.gouv.fr</a>
+                            </li>
+                            <li>
+                                <a href="https://gouvernement.fr">gouvernement.fr</a>
+                            </li>
+                            <li>
+                                <a href="https://service-public.fr">service-public.fr</a>
+                            </li>
+                            <li>
+                                <a href="https://data.gouv.fr">data.gouv.fr</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="rf-footer__bottom">
+                    <ul class="rf-footer__bottom-list">
                         <li>
-                            <a href="https://legifrance.gouv.fr">legifrance.gouv.fr</a>
+                            <a href="https://www.transformation.gouv.fr/donnees-personnelles">Données personelles et cookies</a>
                         </li>
                         <li>
-                            <a href="https://gouvernement.fr">gouvernement.fr</a>
+                            <a href="https://www.transformation.gouv.fr/espace-presse/contact-presse">Contact presse</a>
                         </li>
                         <li>
-                            <a href="https://service-public.fr">service-public.fr</a>
+                            <a href="https://www.transformation.gouv.fr/mentions-legales">Mentions légales</a>
                         </li>
                         <li>
-                            <a href="https://data.gouv.fr">data.gouv.fr</a>
+                            <a href="https://www.transformation.gouv.fr/accessibilite">Accessibilité: non conforme</a>
                         </li>
                     </ul>
+                    <div class="rf-footer__bottom-copy">
+                        Ministère de la Transformation et de la Fonction publiques 2020
+                    </div>
                 </div>
             </div>
-            <div class="rf-footer__bottom">
-                <ul class="rf-footer__bottom-list">
-                    <li>
-                        <a href="https://www.transformation.gouv.fr/donnees-personnelles">Données personelles et cookies</a>
-                    </li>
-                    <li>
-                        <a href="https://www.transformation.gouv.fr/espace-presse/contact-presse">Contact presse</a>
-                    </li>
-                    <li>
-                        <a href="https://www.transformation.gouv.fr/mentions-legales">Mentions légales</a>
-                    </li>
-                    <li>
-                        <a href="https://www.transformation.gouv.fr/accessibilite">Accessibilité: non conforme</a>
-                    </li>
-                </ul>
-                <div class="rf-footer__bottom-copy">
-                    Ministère de la Transformation et de la Fonction publiques 2020
-                </div>
-            </div>
-        </div>
-    </footer>
+        </footer>
+    </div>
 </template>
 
 <script>
@@ -84,6 +90,36 @@
 </script>
 
 <style>
+
+    .subfooter {
+        background-color: #F9F8F6;
+        text-align: left;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 12px 20px;
+    }
+
+    .subfooter p {
+        display: inline-block;
+        margin: 0;
+    }
+
+    .subfooter a {
+        display: inline-block;
+        margin: 0;
+        color: #000091;
+    }
+
+    .subfooter a::after {
+        display: none;
+    }
+
+    .subfooter img {
+        display: inline-block;
+        height: 40px;
+        margin-left: 32px;
+    }
 
     .rf-footer__content-desc {
         display: flex;
