bb22ca8365bca6448d0689eef7b0493a826be9d7
Ugo <jeanmichelpaul@icloud.com>
2020-09-24 11:50:24 +0200

    Add meta on Video page

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="france_relance">
-    
+
     <Header></Header>
-    
+
     <div class="rf-grid-row rf-grid-row--center">
       <div class="rf-col-8">
-      
+
         <div class="rf-grid-row rf-grid-row--center align-items-center">
                 <img src="@/assets/FranceRelance.png" alt="Logo France Relance" class="rf-col-2 mr-5 pt-4" width="110px" >
             <div class="rf-col-9 mt-3">
@@ -44,12 +44,12 @@
                     <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iusto sed, praesentium quia et voluptatum commodi quam. Voluptates, amet illo, alias harum, unde officia eveniet fugit porro aliquam labore quisquam.</p>
                 </div>
             </div>
-        
+
         </div>
 
       </div>
     </div>
-  
+
     <Footer></Footer>
   </div>
 </template>
@@ -62,6 +62,57 @@
          name: "Video",
 
       components: { Header, Footer, },
+
+      data() {
+          return {
+              title: "France Relance : le mot d'Amélie de montchalin - Ministère de la Transformation et de la Fonction publiques",
+              description: "Amélie de Montchalin présente les financements destinés aux administrations dans le cadre du volet Mise à niveau numérique de l'État et des territoires du plan de relance",
+              previewImg: require('@/assets/Preview.png'),
+          }
+      },
+
+      metaInfo () {
+        return {
+          title: this.title,
+          meta: [{
+              name: 'description',
+              content: this.description
+            },
+            {
+              property: 'og:title',
+              content: this.title
+            },
+            {
+              property: 'og:description',
+              content: this.description
+            },
+              {
+              property: 'og:image',
+              content: "https://france-relance.transformation.gouv.fr" + this.previewImg
+            },
+            {
+              name: "twitter:card",
+              content: "summary_large_image"
+            },
+            {
+              name: "twitter:site",
+              content: "@AdeMontchalin"
+            },
+            {
+              name: "twitter:title",
+              content: this.title
+            },
+            {
+              name: "twitter:description",
+              content: this.description
+            },
+            {
+              name: "twitter:image",
+              content: "https://france-relance.transformation.gouv.fr" + this.previewImg
+            },
+          ],
+        }
+      },
 
     }
 </script>
