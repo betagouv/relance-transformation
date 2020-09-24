eaa828833d8524c5e3c3809c19d5a8dab0e98b58
Thibault Jouannic <thibault@miximum.fr>
2020-09-24 12:32:15 +0200

    Use 2 spaces for component indent

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,34 +1,36 @@
 <template>
-    <div class="rf-container">
+  <div class="rf-container">
     <nav class="rf-breadcrumb" aria-labelledby="system-breadcrumb">
-        <h2 id="system-breadcrumb" class="sr-only">Fil d'Ariane</h2>
-        <ul class="rf-breadcrumb__list">
-            <li class="rf-breadcrumb__item">
-                <a href="https://www.transformation.gouv.fr/">
-                    Accueil
-                </a>
-            </li>
+      <h2 id="system-breadcrumb" class="sr-only">Fil d'Ariane</h2>
+      <ul class="rf-breadcrumb__list">
+        <li class="rf-breadcrumb__item">
+          <a href="https://www.transformation.gouv.fr/"> Accueil </a>
+        </li>
 
-            <li v-for="crumb in breadcrumbs" class="rf-breadcrumb__item" v-bind:class="{ 'rf-breadcrumb__item--current': !crumb.url }">
-                <a v-bind:href="crumb.url ? crumb.url : '#'">{{ crumb.name }}</a>
-            </li>
-        </ul>
+        <li
+          v-for="crumb in breadcrumbs"
+          class="rf-breadcrumb__item"
+          v-bind:class="{ 'rf-breadcrumb__item--current': !crumb.url }"
+        >
+          <a v-bind:href="crumb.url ? crumb.url : '#'">{{ crumb.name }}</a>
+        </li>
+      </ul>
     </nav>
-    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Breadcrumbs",
-        data() {
-            return {
-                'breadcrumbs': [],
-            }
-        },
-        created() {
-            this.breadcrumbs = this.$route.meta.breadcrumbs;
-        },
-    }
+export default {
+  name: "Breadcrumbs",
+  data() {
+    return {
+      breadcrumbs: [],
+    };
+  },
+  created() {
+    this.breadcrumbs = this.$route.meta.breadcrumbs;
+  },
+};
 </script>
 
 <style>
