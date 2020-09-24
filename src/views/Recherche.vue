dfe8e70027600b1fe5c29cf265efdc3e7ff5b2ab
Ugo <jeanmichelpaul@icloud.com>
2020-09-24 11:53:52 +0200

    Add meta on Recherche page

--- src/views/Recherche.vue
+++ src/views/Recherche.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="recherche">
-    
+
     <Header></Header>
-    
+
     <div class="rf-grid-row rf-grid-row--center">
       <div class="rf-col-8">
-      
+
         <div class="rf-grid-row rf-grid-row--center align-items-center">
                 <img src="@/assets/FranceRelance.png" alt="Logo France Relance" class="rf-col-2 mr-5 pt-4" width="110px" >
             <div class="rf-col-9 mt-3">
@@ -19,13 +19,13 @@
                     <button class="font20 btn my-2 greenButton my-sm-0 input-group-text pl-4 pr-4" type="submit">Rechercher</button>
                 </div>
                 </div>
-            </form>   
-        
+            </form>
+
         </div>
 
       </div>
     </div>
-  
+
     <Footer></Footer>
   </div>
 </template>
@@ -39,6 +39,57 @@
          name: "Video",
 
       components: { Header, SearchBar, Footer, },
+
+      data() {
+          return {
+              title: "France Relance : rechercher un financement - Ministère de la Transformation et de la Fonction publiques",
+              description: "Rechercher un financement dans le cadre du volet Mise à niveau numérique de l'État et des territoires du plan de relance",
+              previewImg: require('@/assets/Preview.png'),
+          }
+      },
+
+      metaInfo () {
+        return {
+          title: this.title,
+          meta: [{
+              name: 'description',
+              content: this.description
+            },
+            {
+              property: 'og:title',
+              content: this.title
+            },
+            {
+              property: 'og:description',
+              content: this.description
+            },
+              {
+              property: 'og:image',
+              content: "https://france-relance.transformation.gouv.fr" + this.previewImg
+            },
+            {
+              name: "twitter:card",
+              content: "summary_large_image"
+            },
+            {
+              name: "twitter:site",
+              content: "@AdeMontchalin"
+            },
+            {
+              name: "twitter:title",
+              content: this.title
+            },
+            {
+              name: "twitter:description",
+              content: this.description
+            },
+            {
+              name: "twitter:image",
+              content: "https://france-relance.transformation.gouv.fr" + this.previewImg
+            },
+          ],
+        }
+      },
 
     }
 </script>
