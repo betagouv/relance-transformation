5875e6bbe726e9433edd7636734b678607d6a6f6
Ugo <jeanmichelpaul@icloud.com>
2020-09-24 21:57:13 +0200

    Tweak buttons on homepage

--- src/components/DateSelection.vue
+++ src/components/DateSelection.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="rf-col-xs-10 rf-col-sm-6">
         <div role="button" tabindex="0" class="rf-grid-row unselected" :class="{selected:focus}">
-            <div class="rf-col-3">    
+            <div class="rf-col-3 top-selection-image">
                 <img :src="require(`@/assets/picto/${filename}`)" alt="" >
             </div>
             <h3 class="rf-col-9 rf-text">{{ title }}</h3>
         </div>
-    </div>    
+    </div>
 </template>
 
 <script>
@@ -30,23 +30,42 @@ export default {
 </script>
 
 <style>
+
     div[role="button"] {
         height: 8em;
     }
+
     div[role="button"]:hover, div[role="button"]:focus {
-        background-color: #169B62; 
-        color: #fff;        
+        background-color: #169B62;
+        color: #fff;
     }
+
     div[role="button"]:hover h3, div[role="button"]:focus h3 {
         color: #fff;
     }
+
     .selected {
-        background-color: #169B62; 
+        background-color: #169B62;
         color: #fff;
     }
+
     .unselected {
         background-color: #F9F8F6;
         color: #6A6A6A;
+    }
+
+    .top-selection-image {
+        display: flex;
+        align-items: center;
+        padding: 20px !important;
+    }
+
+    .rf-text {
+        margin: 0;
+        padding-left: 0 !important;
+        color: #107449 !important;
+        display: flex;
+        align-items: center;
     }
 
 </style>
