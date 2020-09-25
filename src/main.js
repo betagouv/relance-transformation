023a0704e5a8bcec300945028e88e5b1c48466c3
Thibault Jouannic <thibault@miximum.fr>
2020-09-25 15:39:48 +0200

    Conditionaly enable vue matomo

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import VueMeta from 'vue-meta'
+import VueMatomo from 'vue-matomo'
 
 import store from '@/store'
 import router from '@/router'
 
 Vue.config.productionTip = false
 
-// Vue.use(VueRouter)
 Vue.use(VueMeta)
+
+if (process.env.VUE_APP_USE_ANALYTICS) {
+  Vue.use(VueMatomo, {
+    host: 'https://stats.data.gouv.fr',
+    siteId: process.env.VUE_APP_MATOMO_SITE_ID,
+  });
+}
 
 const vue = new Vue({
   router,
