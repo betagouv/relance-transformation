4a0fb421ae00edc0ba85f5bc42dd1e7e9451693b
Sylvain <sylvain@lazybird.co>
2020-09-25 16:55:37 +0200

    Add sentry performance checks

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from '@/router'
 
 import * as Sentry from "@sentry/browser";
 import { Vue as VueIntegration } from "@sentry/integrations";
+import { Integrations } from "@sentry/tracing";
 
 Vue.config.productionTip = false
 
@@ -21,8 +22,20 @@ const vue = new Vue({
 
 vue.$mount('#app')
 
-
-Sentry.init({
-  dsn: process.env.VUE_APP_SENTRY_DSN,
-  integrations: [new VueIntegration({ Vue, attachProps: true })],
-});
+if (process.env.VUE_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [
+      new Integrations.BrowserTracing(),
+      new VueIntegration({
+        Vue,
+        tracing: true,
+        attachProps: true,
+        tracingOptions: {
+          trackComponents: true,
+        },
+      }),
+    ],
+    tracesSampleRate: 0.1, // percent of transaction that will be transmited.
+  });
+}
