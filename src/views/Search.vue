9c16a7aa39496e3180045b2e50c8de4bb2386a0a
SarahRubio <60744147+SarahRubio@users.noreply.github.com>
2020-09-25 10:35:09 +0200

    delete keydow & ariadescribedby
    Co-authored-by: Thibault J. <thibault@miximum.fr>

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -16,7 +16,7 @@
 
             <form action="" class="searchBar rf-col-11 my-4">
                 <div class="input-group mb-3">
-                <input type="search" class="form-control border-bottom-green formresearch p-4" v-model="newResearch" @keydown.enter.prevent="Research(newResearch)" placeholder="Rechercher un mot clé, une expression, une référence..." aria-describedby="basic-addon2">
+                <input type="search" class="form-control border-bottom-green formresearch p-4" v-model="newResearch" placeholder="Rechercher un mot clé, une expression, une référence…">
                 <div class="input-group-append">
                     <button class="font20 btn my-2 greenButton my-sm-0 input-group-text pl-4 pr-4" type="button" @click="Research(newResearch)">Rechercher</button>
                 </div>
