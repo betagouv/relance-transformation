0f14c6cd8bd62e8b5ec041b1955586da148514e6
SarahRubio <60744147+SarahRubio@users.noreply.github.com>
2020-09-28 10:24:47 +0200

    add space to « ${this.text} »
    Co-authored-by: Thibault J. <thibault@miximum.fr>

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -141,7 +141,7 @@
 
         setTitle(results) {
           if(results.length == 0) {
-            this.title = `Rechercher un financement : Aucun résultat pour «${this.text}» - France Relance - Ministère de la Transformation et de la Fonction publiques`;
+            this.title = `Rechercher un financement : Aucun résultat pour « ${this.text} » – France Relance – Ministère de la Transformation et de la Fonction publiques`;
             this.noResult = true;
           } else if (results.length == 1)  {
             this.title = `Rechercher un financement : ${results.length} résultat pour « ${this.text} » – France Relance – Ministère de la Transformation et de la Fonction publiques`;
