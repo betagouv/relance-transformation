a54b0b3ea67ce48204967c549235c460d1b0b95c
SarahRubio <sarahrubio.pro@gmail.com>
2020-10-05 13:46:08 +0200

    add publication_order parameter for api results

--- src/services/aidService.js
+++ src/services/aidService.js
@@ -4,13 +4,13 @@ export default {
     // Build an api call and fetch it's result
     fetchAidList(querystring) {
 
-        const params = `backers=${process.env.VUE_APP_MTFP_PARAM}&in_france_relance=true&${querystring}`;
+        const params = `backers=${process.env.VUE_APP_MTFP_PARAM}&in_france_relance=true&order_by=publication_date&${querystring}`;
         return api.get('/aids/?' + params);
     },
 
     // Fetch a single aid object
     fetchAidDetail(slug) {
-        const url = 'aids/' + slug + '/'
+        const url = 'aids/?order_by=publication_date' + slug + '/'
         return api.get(url)
             .then(response => response.data);
     }
