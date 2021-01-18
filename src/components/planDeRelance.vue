27e7a454e2f52a8cd6db876a9e587f09d50f356b
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-01-18 10:51:58 +0100

    2 distincts types

--- src/components/planDeRelance.vue
+++ src/components/planDeRelance.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="rf-grid-col rf-grid-col--center infoPlan">
-        <p class="rf-text"><a href="https://www.economie.gouv.fr/plan-de-relance" target="_blank" title="Consulter l’ensemble des mesures du plan de relance du Gouvernement - nouvelle fenêtre">Consulter l’ensemble des mesures du plan de relance du Gouvernement</a>.</p>
-    </div>
+    <p class="rf-text"><a href="https://www.economie.gouv.fr/plan-de-relance" target="_blank" title="Consulter l’ensemble des mesures du plan de relance du Gouvernement - nouvelle fenêtre">Consulter l’ensemble des mesures du plan de relance du Gouvernement</a>.</p>
 </template>
 
 <script>
@@ -9,12 +7,3 @@
         name: "infoPlan",
     }
 </script>
-
-<style>
-    .infoPlan  {
-        background-color: #f9F8F6;
-        margin-top: 3em;
-        padding: 0.5em;
-        text-align: center;
-    }
-</style>
---
+++ src/components/planDeRelanceEtat.vue
@@ -0,0 +1,9 @@
+<template>
+         <p class="rf-text">Besoin d'aide ? Écrivez-nous : <a href="mailto:pmo.francerelance@modernisation.gouv.fr">pmo.francerelance@modernisation.gouv.fr</a>.</p>
+</template>
+
+<script>
+    export default {
+        name: "infoPlanEtat",
+    }
+</script>
