928e7d446ca8f3722cbe13169dba7d465b4cf578
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-01-18 09:33:28 +0100

    delete "need help"

--- src/components/planDeRelance.vue
+++ src/components/planDeRelance.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="rf-grid-col rf-grid-col--center infoPlan">
-        <p>Besoin d'aide ? Écrivez-nous : <a href="mailto:pmo.francerelance@modernisation.gouv.fr">pmo.francerelance@modernisation.gouv.fr</a>.</p>
         <p class="rf-text"><a href="https://www.economie.gouv.fr/plan-de-relance" target="_blank" title="Consulter l’ensemble des mesures du plan de relance du Gouvernement - nouvelle fenêtre">Consulter l’ensemble des mesures du plan de relance du Gouvernement</a>.</p>
     </div>
 </template>
