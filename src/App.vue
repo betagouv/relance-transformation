d45eec851feb2c7f71a5ec063d382e59cb0ed472
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-03-15 17:52:36 +0100

    cleaning color

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,6 @@
   font-family: 'Marianne','Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
-  /* margin-top: 60px; */
 }
 
 @font-face {
